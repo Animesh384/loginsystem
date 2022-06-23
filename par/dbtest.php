645d70e903dd86bc515077898dadfedf2552f9a0
akanoob <animeshk384@gmail.com>
2022-06-23 14:40:34 +0530

    config file update!

--- par/dbtest.php
+++ par/dbtest.php
@@ -1,25 +1,4 @@
  <?php
-/* $r=mysqli_query($con,$sql);
-  $num=mysqli_num_rows($r);
-  if($num==1){
-      echo "$sql";
-      //while($row=mysqli_fetch_array($r)){echo"{$row['name']}"."{$row['username']}"."{$row['password']}"."{$row['gender']}"."{$row['email']}";  }
-      session_start();
-      $_SESSION['loggedin']=true;
-      $_SESSION['username']=$uname;
-      $_SESSION['R=result']=mysqli_fetch_array($r);
-      header("location:welecome.php");
-  }
-  else {
-  echo"no non npn npn no";
-  $_SESSION['loggedin']=false;
-  }
-  $sql="select * from user where username='$uname'AND password='$pass'";
-  $r=mysqli_query($con,$sql);
-  $num=mysqli_num_rows($r);
-  */
-
-
 
 $ser="localhost";
 $nam="root";
@@ -31,28 +10,36 @@ if($con){
 }
 else {
   die("error".msqli_error());
-} 
+}
 
 $quer="create db";
 if($con->query("use db")==TRUE){
     $con=mysqli_connect($ser,$nam,$pass,$db);
-    echo "hello sucess";
+    echo "successfully link to data base";
 }
 else{
-    echo "hello sucshvsvsss";
+    //creating database
     if($con->query("create database db")==TRUE){
     $con=mysqli_connect($ser,$nam,$pass,$db);
+    echo "connection made successfully\n";
     }
     if($con->query("select * from user")>0){
-
-        }
+      echo "table already created";
+    }
     else{
+      //creating table
       $myquery="create table if not exists user(name varchar(20),username varchar(25),password varchar(26),gender varchar(10),email varcahr(35))";
-      $con->query($myquery);
-    }    
+      //making query to create table user
+      if($con->query($myquery)){
+        echo "table created";
+      }
+      else{
+        die("failed reasion-->".mysqli_error());
+      }
+    }
 
 
 
 }
-  
+
   ?>
